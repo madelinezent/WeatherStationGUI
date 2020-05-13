16002d633325788534c2120fc1c0761dc3bb04dc
Ali Iftakhar <49818898+ali92115@users.noreply.github.com>
2020-05-13 14:55:17 -0700

    Updated location.

---
+++ PRJ#02-Group1/src/sensorData/Sensors7.java
@@ -0,0 +1,62 @@
+package sensorData;
+import sensorSuite7.SensorSuite;
+/**
+ * Main program.
+ */
+public class Sensors7 implements ConsoleSensor {
+    private static SensorSuite sensorSuite;
+
+    public void Initialize() {
+        sensorSuite = new SensorSuite();
+        try {
+			startSensorSuite();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public double rainFall() {
+    	return sensorSuite.getSensors().get(0).getData();
+    }
+    
+    public double rainRate() {
+    	return -1;
+    }
+    
+    public double humOut() {
+    	return sensorSuite.getSensors().get(2).getData();
+    }
+    
+    public double humIn() {
+    	return -1;
+    }
+    
+    public double tempOut() {
+    	return sensorSuite.getSensors().get(1).getData();
+    }
+    
+    public double tempIn() {
+    	return -1;
+    }
+    
+    public double windDirection() {
+    	return -1;
+    }
+    
+    public double windSpeed() {
+    	return sensorSuite.getSensors().get(3).getData();
+    }
+    
+    public double pressure() {
+    	return -1;
+    }
+
+    public double windchill() {
+    	return -1;
+    }
+    
+    private static void startSensorSuite() throws InterruptedException {
+        sensorSuite.run();
+    }
+}
