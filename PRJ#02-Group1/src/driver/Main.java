b65048c5ccfd6d9d68aa4625625b7557b8e4a6db
Ali Iftakhar <49818898+ali92115@users.noreply.github.com>
2020-05-16 16:38:25 -0700

    Made a running program in main.

--- PRJ#02-Group1/src/driver/Main.java
+++ PRJ#02-Group1/src/driver/Main.java
@@ -1,10 +1,19 @@
 package driver;
+import java.awt.EventQueue;
+
+import consoleView.*;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Startup window = new Startup();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
